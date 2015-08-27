fed8bcaabc165cdf00f7cb0e0def8758d8b9a8d0
Aleksander Grzybowski <kelog@orange.pl>
2015-08-27 10:48:49 +0200

    Start with nearly empty RBT

--- app/config.js
+++ app/config.js
@@ -4,5 +4,5 @@ var CONFIG = {
     levelsScalingFactor: 10,
     defaultBSTElements: [80, 40, 120, 20, 60, 10, 120, 140, 130, 100, 110, 150],
     defaultHeapElements: [10, 20, 30, 40, 50, 60, 70, 80, 90],
-    defaultRBTElements: [5, 1, 8]
+    defaultRBTElements: [5]
 };
