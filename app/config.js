80b210a3489cf66cb5cc129221791504822b98f3
Aleksander Grzybowski <kelog@orange.pl>
2015-11-20 17:31:45 +0100

    Comma for easy uncommenting

--- app/config.js
+++ app/config.js
@@ -1,10 +1,11 @@
+//noinspection JSLastCommaInObjectLiteral,JSDuplicatedDeclaration
 var CONFIG = {
     circleRadius: 80,
 
     defaultBSTElements: [],
     defaultHeapElements: [],
     defaultRBTElements: [],
-    defaultSplayTreeElements: []
+    defaultSplayTreeElements: [],
    
     // used for manual testing
     //defaultBSTElements: [80, 40, 120, 20, 60, 10, 120, 140, 130, 100, 110, 150],
