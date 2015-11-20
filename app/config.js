36b2f35d061031ac9372247ed7317a8306af3eb7
Aleksander Grzybowski <kelog@orange.pl>
2015-11-20 17:19:49 +0100

    Empty trees by default

--- app/config.js
+++ app/config.js
@@ -1,7 +1,14 @@
 var CONFIG = {
     circleRadius: 80,
-    defaultBSTElements: [80, 40, 120, 20, 60, 10, 120, 140, 130, 100, 110, 150],
-    defaultHeapElements: [10, 20, 30, 40, 50, 60, 70, 80, 90],
-    defaultRBTElements: [5],
-    defaultSplayTreeElements: [2, 1]
+
+    defaultBSTElements: [],
+    defaultHeapElements: [],
+    defaultRBTElements: [],
+    defaultSplayTreeElements: []
+   
+    // used for manual testing
+    //defaultBSTElements: [80, 40, 120, 20, 60, 10, 120, 140, 130, 100, 110, 150],
+    //defaultHeapElements: [10, 20, 30, 40, 50, 60, 70, 80, 90],
+    //defaultRBTElements: [5],
+    //defaultSplayTreeElements: [2, 1]
 };
