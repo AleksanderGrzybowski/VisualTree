b27345ff0790a87906c610e73429fb34cb490583
Aleksander Grzybowski <kelog@orange.pl>
2016-01-05 16:51:32 +0100

    Working IntelliJ TS compilation, BST rewritten

--- app/bst/BSTNode.ts
+++ app/bst/BSTNode.ts
@@ -1,28 +1,41 @@
-var BSTNode = (function () {
-    function BSTNode(value) {
+declare var SNC:any;
+
+class BSTNode {
+
+    constructor(value:number) {
         this.left = null;
         this.right = null;
         this.parent = null;
         this.value = value;
         this.visual = '';
     }
-    BSTNode.prototype.isLeaf = function () {
+
+    left:BSTNode;
+    right:BSTNode;
+    parent:BSTNode;
+
+    value:number;
+    visual:string;
+
+    isLeaf():boolean {
         return this.left === null && this.right === null;
     };
-    ;
-    BSTNode.prototype.findRoot = function () {
-        var node = this;
+
+    findRoot():BSTNode {
+        var node:BSTNode = this;
+
         while (node.parent !== null) {
             node = node.parent;
         }
+
         return node;
     };
-    ;
-    BSTNode.prototype.height = function () {
+
+
+    height():number {
         if (this.left === null && this.right === null) {
             return 1;
-        }
-        else {
+        } else {
             var heightLeft = 0;
             var heightRight = 0;
             if (this.left !== null) {
@@ -34,201 +47,220 @@ var BSTNode = (function () {
             return Math.max(heightLeft, heightRight);
         }
     };
-    ;
-    BSTNode.prototype.add = function (value) {
+
+    add(value:number):BSTNode {
         this.visual = 'intermediate';
         SNC.add('Visiting node ' + this.value);
+
         if (value < this.value) {
             if (this.left === null) {
                 this.left = new BSTNode(value);
                 this.left.parent = this;
+
                 this.left.visual = 'current';
                 SNC.add('Creating new on the left');
                 this.left.visual = '';
                 SNC.add('Done');
-            }
-            else {
+            } else {
                 SNC.add('Item to add is smaller than current, going left');
                 this.left.add(value);
             }
-        }
-        else if (value > this.value) {
+        } else if (value > this.value) {
             if (this.right === null) {
                 this.right = new BSTNode(value);
                 this.right.parent = this;
+
                 this.right.visual = 'current';
                 SNC.add('Creating new on the right');
                 this.right.visual = '';
                 SNC.add('Done');
-            }
-            else {
+            } else {
                 SNC.add('Item to add is larger than current, going right');
                 this.right.add(value);
             }
-        }
-        else {
+        } else {
             this.visual = 'current';
             SNC.add('Found duplicate, nothing to do.');
             this.visual = '';
             SNC.add('Found duplicate, nothing to do.');
         }
+
         this.visual = '';
         SNC.add('Going back');
+
         return this;
     };
-    ;
-    BSTNode.prototype.inorder = function () {
+
+    inorder() {
         if (this.left !== null) {
             this.visual = 'intermediate';
             SNC.add('Going left');
+
             this.left.inorder();
         }
+
         this.visual = 'current';
         SNC.add('Visiting ' + this.value);
+
         if (this.right !== null) {
             this.visual = 'intermediate';
             SNC.add('Going right');
+
             this.right.inorder();
         }
+
         this.visual = '';
         SNC.add('Going back');
     };
-    ;
-    BSTNode.prototype.preorder = function () {
+
+    preorder() {
         this.visual = 'current';
         SNC.add('Visiting ' + this.value);
+
         if (this.left !== null) {
             this.visual = 'intermediate';
             SNC.add('Going left');
+
             this.left.preorder();
         }
+
         if (this.right !== null) {
             this.visual = 'intermediate';
             SNC.add('Going right');
+
             this.right.preorder();
         }
+
         this.visual = '';
         SNC.add('Going back');
     };
-    ;
-    BSTNode.prototype.postorder = function () {
+
+    postorder() {
         if (this.left !== null) {
             this.visual = 'intermediate';
             SNC.add('Going left');
+
             this.left.postorder();
         }
+
         if (this.right !== null) {
             this.visual = 'intermediate';
             SNC.add('Going right');
+
             this.right.postorder();
         }
         this.visual = 'current';
         SNC.add('Visiting ' + this.value);
+
         this.visual = '';
         SNC.add('Going back');
     };
-    ;
-    BSTNode.prototype.minValue = function () {
+
+    minValue() {
         if (this.left === null) {
             return this.value;
-        }
-        else {
+        } else {
             return this.left.minValue();
         }
     };
-    ;
-    BSTNode.prototype.delete = function (value) {
+
+
+    delete(value:number) {
         this.visual = 'intermediate';
         SNC.add('Is this the one to delete? ' + this.value);
+
         if (this.value !== value) {
             SNC.add('This is not the one to delete, going left or right?');
+
             if (value < this.value) {
                 SNC.add('Going left');
+
                 if (this.left !== null) {
                     this.left.delete(value);
                     this.visual = '';
                     SNC.add('Going back');
-                }
-                else {
+                } else {
                     SNC.add('Node not found on the left');
                     this.visual = '';
                     SNC.add('Going back');
                 }
-            }
-            else if (value > this.value) {
+            } else if (value > this.value) {
                 SNC.add('Going right');
+
                 if (this.right !== null) {
                     this.right.delete(value);
                     this.visual = '';
                     SNC.add('Going back');
-                }
-                else {
+                } else {
                     SNC.add('Node not found on the right');
                     this.visual = '';
                     SNC.add('Going back');
                 }
             }
-        }
-        else {
+        } else {
             // TODO which equals?
             // remember where ref and where val
+
             this.visual = 'current';
             SNC.add('This is the one to delete');
+
             if (this.left === null && this.right === null) {
                 SNC.add('No children - removing!');
+
                 // remove itself using parent link, but
                 // must know if is is left or right
                 // every time
+
                 if (this === this.parent.left) {
                     this.parent.left = null;
-                }
-                else if (this === this.parent.right) {
+                } else if (this === this.parent.right) {
                     this.parent.right = null;
-                }
-                else {
+                } else {
                     throw new Error();
                 }
+
                 SNC.add('Done');
-            }
-            else if (this.left === null && this.right !== null) {
+            } else if (this.left === null && this.right !== null) {
                 SNC.add('Child on the right - removing!');
+
                 if (this === this.parent.left) {
                     this.right.parent = this.parent;
                     this.parent.left = this.right;
-                }
-                else if (this === this.parent.right) {
+                } else if (this === this.parent.right) {
                     this.right.parent = this.parent;
                     this.parent.right = this.right;
-                }
-                else {
+                } else {
                     throw new Error();
                 }
+
                 SNC.add('Done');
-            }
-            else if (this.left !== null && this.right === null) {
+            } else if (this.left !== null && this.right === null) {
                 SNC.add('Child on the left - removing!');
+
                 if (this === this.parent.left) {
                     this.left.parent = this.parent;
                     this.parent.left = this.left;
-                }
-                else if (this === this.parent.right) {
+                } else if (this === this.parent.right) {
                     this.left.parent = this.parent;
                     this.parent.right = this.left;
-                }
-                else {
+                } else {
                     throw new Error();
                 }
+
                 SNC.add('Done');
-            }
-            else {
+            } else {
                 //http://www.algolist.net/Data_structures/Binary_search_tree/Removal
+
                 SNC.add('Searching for minimum value in the right subtree...');
                 var min = this.right.minValue();
+
                 SNC.add('We have minimum ' + min);
+
                 // replace value of the node to be removed with found min
                 this.value = min;
                 SNC.add('Replace current node value with found minimum');
                 // apply remove to the right subtree to remove a duplicate
+
                 this.visual = 'intermediate';
                 SNC.add('Running remove recursively on the right subtree');
                 this.right.delete(min);
@@ -237,7 +269,4 @@ var BSTNode = (function () {
             }
         }
     };
-    ;
-    return BSTNode;
-})();
-//# sourceMappingURL=BSTNode.js.map
+}
--- app/bst/BSTree.ts
+++ app/bst/BSTree.ts
@@ -1,85 +1,87 @@
 /// <reference path="BSTNode.ts" />
-var BSTree = (function () {
-    function BSTree() {
+declare var SNC:any;
+
+class BSTree {
+
+    constructor() {
         this.root = null;
     }
-    BSTree.prototype.add = function (value) {
+
+    root:BSTNode;
+
+    add(value:number) {
         if (this.root === null) {
             this.root = new BSTNode(value);
             SNC.add('Adding root');
-        }
-        else {
+        } else {
             this.root.add(value);
         }
     };
-    ;
-    BSTree.prototype.addAll = function (elements) {
+
+    addAll(elements:number[]) {
         for (var i = 0; i < elements.length; ++i) {
             this.add(elements[i]);
         }
     };
-    ;
-    BSTree.prototype.height = function () {
+
+    height() {
         if (this.root === null) {
             return 0;
-        }
-        else {
+        } else {
             return this.root.height();
         }
     };
-    ;
-    BSTree.prototype.postorder = function () {
+
+    postorder() {
         if (this.root !== null) {
             this.root.postorder();
         }
     };
-    ;
-    BSTree.prototype.preorder = function () {
+
+    preorder() {
         if (this.root !== null) {
             this.root.preorder();
         }
     };
-    ;
-    BSTree.prototype.inorder = function () {
+
+    inorder () {
         if (this.root !== null) {
             this.root.inorder();
         }
     };
-    ;
-    BSTree.prototype.delete = function (value) {
-        if (this.root.value === value) {
+
+    delete(value: number) {
+        if (this.root.value === value) { // removing root
             this.root.visual = 'current'; // no need to revert it - will be deleted
             SNC.add('Deleting root');
             if (this.root.left === null && this.root.right === null) {
                 // TODO here root is not preserved, so SNC can't track it...
                 this.root = null;
                 SNC.add('');
-            }
-            else if (this.root.left === null && this.root.right !== null) {
+            } else if (this.root.left === null && this.root.right !== null) {
                 SNC.add('Child on the right - removing!');
+
                 this.root = this.root.right;
                 this.root.parent = null;
+
                 SNC.add('Done');
-            }
-            else if (this.root.left !== null && this.root.right === null) {
+            } else if (this.root.left !== null && this.root.right === null) {
                 SNC.add('Child on the left - removing!');
+
                 this.root = this.root.left;
                 this.root.parent = null;
+
                 SNC.add('Done');
-            }
-            else {
+            } else {
                 // we can delegate to BSTNode.delete, cause
                 // deletion of 2-children-element
                 // modifies value of root, links to root
                 // are preserved
+
                 this.root.delete(value);
             }
-        }
-        else {
+        } else {
             this.root.delete(value); // will surely go left or right
         }
     };
-    ;
-    return BSTree;
-})();
-//# sourceMappingURL=BSTree.js.map
+}
